af3769b69837a4299a124d8adf18266d1eb9b4a8
YJC <yjcyxky@163.com>
2019-07-15 14:17:23 +0800

    First Commit.

---
+++ doc/intro.md
@@ -0,0 +1,67 @@
+# Introduction to cgdsclj
+
+Clojure based API for accessing the Cancer Genomics Data Server (CGDS).
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Introduction
+
+cgdsclj based API for accessing the Cancer Genomics Data Server (CGDS). It's based on [cgdsr](https://github.com/cBioPortal/cgdsr). Queries the CGDS API and returns available cancer studies. Input is a CGDS object and output is an array with information regarding the different cancer studies.
+
+## Installation
+
+`cgdsclj` is available as a Maven artifact from Clojars.
+
+With Leiningen/Boot:
+
+```
+[cgdsclj "0.1.0"]
+```
+
+cgdsclj supports clojure 1.8.0 and higher.
+
+## Usage
+
+To use cgdsclj:
+
+```clojure
+(require '[cgdsclj "0.1.0" as cgds])
+
+(def base-url "http://www.cbioportal.org/")
+
+# get cancer study
+(def cancer-studies (cgds/cancer-studies base-url))
+(cgds/list->map cancer-studies)
+
+# get case list
+(case-lists base-url "um_qimr_2016")
+
+# get genetic profile
+(genetic-profiles base-url "um_qimr_2016")
+```
+
+## Contact
+
+Jingcheng Yang
+yjcyxky@163.com
+
+Your feedbacks are welcome. If you're struggling using the librairy, the best way to ask questions is to use the Github issues so that they are shared with everybody.
+
+## License
+
+Copyright © 2019 Choppy Team. https://github.com/orgs/go-choppy/teams/choppy-team
+
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
+
+This Source Code may also be made available under the following Secondary
+Licenses when the conditions for such availability set forth in the Eclipse
+Public License, v. 2.0 are satisfied: GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or (at your
+option) any later version, with the GNU Classpath Exception which is available
+at https://www.gnu.org/software/classpath/license.html.
---
+++ src/cgdsclj/core.clj
@@ -0,0 +1,110 @@
+(ns cgdsclj.core
+  (:gen-class)
+  (:require [clj-http.client "3.10.0" :as client]
+            [clj-toml "0.3.1" :as toml]))
+
+(defn headers []
+  {})
+
+(defn body []
+  {})
+
+(defn json-req-header [& {:keys [conn-timeout] :or {conn-timeout 1000}}]
+  "Generate a header for json request."
+  {:content-type :json
+   :connection-timeout conn-timeout
+   :accept :json})
+
+(defn process-url [url]
+  "Get data from a specified url."
+  (client/get url (into {} [(json-req-header), (headers), (body)])))
+
+(defn strip-slash [url]
+  "Strip slash from a string"
+  (clojure.string/replace url #"/$" ""))
+
+(defn new-url [url path & more-path]
+  "Join url with path."
+  (str (strip-slash url) path (clojure.string/join more-path)))
+
+(defn string->list [string sep]
+  "Convert string to a list."
+  (clojure.string/split string sep))
+
+(defn list->map [list]
+  "Convert list to map"
+  (let [header (first list)
+        body (rest list)]
+    (map #(zipmap (map keyword header) %) body)))
+
+(defn join-str-or-list
+  ([str-or-list] (join-str-or-list "," str-or-list))
+  ([sep str-or-list] (clojure.string/join sep (concat str-or-list))))
+
+(defn body-data [url]
+  (:body (process-url url)))
+
+(def not-nil? (complement nil?))
+
+(defn cancer-studies [url]
+  "Get cancer studies from data portal."
+  (let [body (body-data (new-url url "/webservice.do?cmd=getCancerStudies&"))]
+    (map #(string->list % #"\t") (string->list body #"\n"))))
+
+(defn case-lists [url cancer-study-id]
+  "Get case list from data portal"
+  (let [body (body-data
+              (new-url url "/webservice.do?cmd=getCaseLists"
+                       "&cancer_study_id=" cancer-study-id))]
+    (map #(string->list % #"\t") (string->list body #"\n"))))
+
+(defn genetic-profiles [url cancer-study-id]
+  "Get genetic profiles from data portal"
+  (let [body (body-data (new-url url "/webservice.do?cmd=getGeneticProfiles"
+                                 "&cancer_study_id=" cancer-study-id))]
+    (map #(string->list % #"\t") (string->list body #"\n"))))
+
+(defn mutation-data [url cancer-study-id case-list-id genetic-profile-id gene-list]
+  "Get mutation data from data portal"
+  (let [body (body-data
+              (new-url url "/webservice.do?cmd=getMutationData"
+                       "&cancer_study_id=" cancer-study-id
+                       "&case_set_id=" case-list-id
+                       "&genetic_profile_id=" genetic-profile-id
+                       "&gene_list=" (join-str-or-list "," gene-list)))]
+    (map #(string->list % #"\t") (string->list body #"\n"))))
+
+(defn profile-data [url gene-list genetic-profiles case-list cases case-ids-key]
+  "Get profile data from data portal"
+  (let [url (new-url url "webservice.do?cmd=getProfileData"
+                     "&gene_list" (join-str-or-list "," gene-list)
+                     "&genetic_profile_id=" (join-str-or-list "," genetic-profiles)
+                     "&id_type" "gene_symbol")]
+    (cond (> (count cases) 0)
+          (let [body (body-data
+                      (new-url url "&case_list=" (join-str-or-list "," cases)))]
+            (map #(string->list % #"\t") (string->list body #"\n")))
+          (> (count case-ids-key) 0)
+          (let [body (body-data
+                      (new-url url "&case_ids_key=" case-ids-key))]
+            (map #(string->list % #"\t") (string->list body #"\n")))
+          :else
+          (let [body (body-data
+                      (new-url url "&case_set_id" case-list))]
+            (map #(string->list % #"\t") (string->list body #"\n"))))))
+
+(defn clinical-data [url case-list cases case-ids-key]
+  "Get clinical data from data portal"
+  (let [url (new-url url "webservice.do?cmd=getProfileData")]
+    (cond (> (count cases) 0)
+          (let [body (body-data
+                      (new-url url "&case_list=" (join-str-or-list "," cases)))]
+            (map #(string->list % #"\t") (string->list body #"\n")))
+          (> (count case-ids-key) 0)
+          (let [body (body-data
+                      (new-url url "&case_ids_key=" case-ids-key))]
+            (map #(string->list % #"\t") (string->list body #"\n")))
+          :else
+          (let [body (body-data
+                      (new-url url "&case_set_id" case-list))]
+            (map #(string->list % #"\t") (string->list body #"\n"))))))
