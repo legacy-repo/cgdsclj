d19f8ac45847aec1010bc6702808608416677ed2
YJC <yjcyxky@163.com>
2019-07-19 11:04:16 +0800

    Make the code more functional.

--- src/cgdsclj/core.clj
+++ src/cgdsclj/core.clj
@@ -1,8 +1,6 @@
 (ns cgdsclj.core
   "Clojure based API for accessing the Cancer Genomics Data Server (CGDS)."
-  (:require [clj-http.client :as client])
-  (:use [slingshot.slingshot :only [throw+ try+]])
-  (:use [clojure.tools.logging :as log]))
+  (:require [clj-http.client :as client]))
 
 (def headers
   {:content-type :json
@@ -43,54 +41,47 @@
 
 (defn query-data [url headers]
   "Get data from a specified url."
-  (try+
-   (client/get url headers)
-   (catch [:status 403] {:keys [request-time headers body]}
-     (log/warn "403" request-time headers))
-   (catch [:status 404] {:keys [request-time headers body]}
-     (log/warn "NOT Found 404" request-time headers body))
-   (catch Object _
-     (log/error (:throwable &throw-context) "unexpected error")
-     (throw+))))
+  (client/get url headers))
 
 (defn body-data
+  "Get body data from a http response map."
   [url headers] (:body (query-data url headers)))
+
+(defn string->nested-list [string]
+  "Convert string that seperated by tab and enter to a nested list."
+  (map #(string->list % #"\t") (string->list string #"\n")))
 
 (def not-nil? (complement nil?))
 
 (defn cancer-studies [url]
   "Get cancer studies from data portal."
-  (let [body (body-data
-              (new-url url "/webservice.do?cmd=getCancerStudies")
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getCancerStudies")
+      (body-data headers)
+      (string->nested-list)))
 
 (defn case-lists [url cancer-study-id]
   "Get case list from data portal"
-  (let [body (body-data
-              (new-url url "/webservice.do?cmd=getCaseLists"
-                       "&cancer_study_id=" cancer-study-id)
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getCaseLists"
+               "&cancer_study_id=" cancer-study-id)
+      (body-data headers)
+      (string->nested-list)))
 
 (defn genetic-profiles [url cancer-study-id]
   "Get genetic profiles from data portal"
-  (let [body (body-data
-              (new-url url "/webservice.do?cmd=getGeneticProfiles"
-                       "&cancer_study_id=" cancer-study-id)
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getGeneticProfiles"
+               "&cancer_study_id=" cancer-study-id)
+      (body-data headers)
+      (string->nested-list)))
 
 (defn mutation-data [url cancer-study-id case-list-id genetic-profile-id genes]
   "Get mutation data from data portal"
-  (let [body (body-data
-              (new-url url "/webservice.do?cmd=getMutationData"
-                       "&cancer_study_id=" cancer-study-id
-                       "&case_set_id=" case-list-id
-                       "&genetic_profile_id=" genetic-profile-id
-                       "&gene_list=" (clojure.string/join genes))
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getMutationData"
+               "&cancer_study_id=" cancer-study-id
+               "&case_set_id=" case-list-id
+               "&genetic_profile_id=" genetic-profile-id
+               "&gene_list=" (clojure.string/join genes))
+      (body-data headers)
+      (string->nested-list)))
 
 (def case-type-map {:case-list "&case_list="
                     :case-ids-key "&case_ids_key="
@@ -98,23 +89,19 @@
 
 (defn profile-data [url genes genetic-profiles cases case-type]
   "Get profile data from data portal"
-  (let [url (new-url url "/webservice.do?cmd=getProfileData"
-                     "&gene_list=" (clojure.string/join genes)
-                     "&genetic_profile_id=" (clojure.string/join genetic-profiles)
-                     "&id_type=" "gene_symbol")
-        body (body-data
-              (new-url url
-                       (case-type case-type-map)
-                       (clojure.string/join cases))
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getProfileData"
+               "&gene_list=" (clojure.string/join genes)
+               "&genetic_profile_id=" (clojure.string/join genetic-profiles)
+               "&id_type=" "gene_symbol"
+               (case-type case-type-map)
+               (clojure.string/join cases))
+      (body-data headers)
+      (string->nested-list)))
 
 (defn clinical-data [url cases case-type]
   "Get clinical data from data portal"
-  (let [url (new-url url "/webservice.do?cmd=getClinicalData")
-        body (body-data
-              (new-url url
-                       (case-type case-type-map)
-                       (clojure.string/join cases))
-              headers)]
-    (map #(string->list % #"\t") (string->list body #"\n"))))
+  (-> (new-url url "/webservice.do?cmd=getClinicalData"
+               (case-type case-type-map)
+               (clojure.string/join cases))
+      (body-data headers)
+      (string->nested-list)))
